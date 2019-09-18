7cd583b8afd69228fe4a6851451b32411351a956
Joel Denning <joeldenning@gmail.com>
2019-09-18 09:45:10 -0600

    MF-56: Add logout to primary nav bar (#5)
    * MF-56: Add logout to primary nav bar
    
    * Review changes
    
    * Fix breaking build

--- src/root.component.test.tsx
+++ src/root.component.test.tsx
@@ -1,11 +1,80 @@
 import React from "react";
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, fireEvent, wait } from "@testing-library/react";
 import Root from "./root.component";
+import { of } from "rxjs";
+import { getCurrentUser } from "@openmrs/esm-api";
+import { act } from "react-dom/test-utils";
+import { createMemoryHistory } from "history";
+import { Router } from "react-router";
 
+function renderWithRouter(
+  ui,
+  {
+    route = "/",
+    history = createMemoryHistory({ initialEntries: [route] }),
+    match = { params: {}, isExact: true, path: "", url: "" }
+  } = {}
+) {
+  return {
+    ...render(<Router history={history}>{ui}</Router>),
+    history,
+    match
+  };
+}
+
+const mockGetCurrentUser = getCurrentUser as jest.Mock;
+window["getOpenmrsSpaBase"] = jest.fn().mockImplementation(() => "/");
 afterAll(cleanup);
+
+const mockUser = {
+  authenticated: true,
+  user: {
+    uuid: "uuid",
+    display: "admin",
+    person: { uuid: "uuid", display: "Test User" },
+    privileges: [],
+    roles: [{ uuid: "uuid", display: "System Developer" }]
+  }
+};
+
+jest.mock("@openmrs/esm-api", () => ({
+  openmrsFetch: jest.fn().mockResolvedValue({}),
+  getCurrentUser: jest
+    .fn()
+    .mockImplementation(() => ({ subscribe: () => {}, unsubscribe: () => {} }))
+}));
 
 describe(`<Root />`, () => {
   it(`renders without dying`, () => {
-    const wrapper = render(<Root />);
+    let wrapper;
+    act(() => {
+      wrapper = render(<Root />);
+    });
+  });
+
+  it(`renders avatar`, async () => {
+    mockGetCurrentUser.mockImplementation(() => of(mockUser));
+    let wrapper;
+    act(() => {
+      wrapper = render(<Root />);
+    });
+    await wait(() => expect(wrapper.getByText("TU")).not.toBeNull());
+  });
+
+  it(`logs out patient`, async () => {
+    mockGetCurrentUser.mockImplementation(() => of(mockUser));
+    let wrapper;
+    act(() => {
+      wrapper = renderWithRouter(<Root />);
+    });
+    const userMenuBtn = wrapper.container.querySelector(".avatar");
+    act(() => {
+      fireEvent.click(userMenuBtn);
+    });
+    const logoutBtn = wrapper.getByText(/Logout/i);
+    expect(logoutBtn).not.toBeNull();
+    act(() => {
+      fireEvent.click(logoutBtn);
+    });
   });
 });
--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,9 +1,33 @@
 import React from "react";
 import openmrsRootDecorator from "@openmrs/react-root-decorator";
 import styles from "./root.styles.css";
+import { toggle } from "kremling";
+import { BrowserRouter, Redirect } from "react-router-dom";
+import { getCurrentUser, openmrsFetch } from "@openmrs/esm-api";
 
-function Root() {
+function Root(props: NavProps) {
   const [sidenavOpen, setSidenavOpen] = React.useState(false);
+  const [isUserMenuOpen, setIsUserMenuOpen] = React.useState(false);
+  const [userNames, setUserNames] = React.useState(null);
+  const [userInitials, setUserInitials] = React.useState(null);
+  const [isLoggedIn, setIsLoggedIn] = React.useState(true);
+  const [isLoggingOut, setIsLoggingOut] = React.useState(false);
+
+  React.useEffect(() => {
+    const sub = getCurrentUser({ includeAuthStatus: true }).subscribe(user => {
+      if (user.authenticated) {
+        setIsLoggedIn(true);
+        const { display } = user.user.person;
+        setUserNames(display);
+        setUserInitials(
+          `${display.split(/\s/)[0][0]}${display.split(/\s/)[1][0]}`
+        );
+      } else {
+        setIsLoggedIn(false);
+      }
+    });
+    return () => sub.unsubscribe();
+  }, []);
 
   React.useEffect(() => {
     const operation = sidenavOpen ? "add" : "remove";
@@ -14,28 +38,72 @@ function Root() {
     };
   }, [sidenavOpen]);
 
+  React.useEffect(() => {
+    const ac = new AbortController();
+    if (isLoggingOut) {
+      openmrsFetch("/ws/rest/v1/session", {
+        method: "DELETE"
+      }).then(() => setIsLoggedIn(false));
+    }
+    return () => ac.abort();
+  }, [isLoggingOut]);
+
   return (
-    <nav className={styles.topNav}>
-      <button
-        className="omrs-unstyled omrs-padding-left-4 omrs-padding-right-4"
-        onClick={toggleSidenav}
-      >
-        <svg className={`omrs-icon ${styles.menuIcon}`}>
-          <use xlinkHref="#omrs-icon-menu" />
-        </svg>
-      </button>
-      <div className="omrs-type-title-4">
-        {/* We'll figure out whether to use document.title or not later */}
-        Home
-      </div>
-      <div className={styles.avatar}>AB</div>
-    </nav>
+    <BrowserRouter>
+      <>
+        {!isLoggedIn && (
+          <Redirect
+            // @ts-ignore
+            to={`${window.getOpenmrsSpaBase()}login`}
+          />
+        )}
+        <nav className={styles.topNav}>
+          <button
+            className="omrs-unstyled omrs-padding-left-4 omrs-padding-right-4"
+            onClick={toggleSidenav}
+          >
+            <svg className={`omrs-icon ${styles.menuIcon}`}>
+              <use xlinkHref="#omrs-icon-menu" />
+            </svg>
+          </button>
+          <div className="omrs-type-title-4">
+            {/* We'll figure out whether to use document.title or not later */}
+            Home
+          </div>
+          <div>
+            <button
+              onClick={() => setIsUserMenuOpen(!isUserMenuOpen)}
+              className={`omrs-unstyled ${styles.avatar}`}
+            >
+              {userInitials}
+            </button>
+          </div>
+        </nav>
+        <div className={toggle(styles.userMenu, styles.hidden, isUserMenuOpen)}>
+          <div className={`${styles.userMenuCard} omrs-padding-16`}>
+            <div className="omrs-type-body-large omrs-margin-12">
+              {userNames}
+            </div>
+            <button
+              onClick={() => setIsLoggingOut(true)}
+              className="omrs-btn omrs-outlined-neutral"
+            >
+              Logout
+            </button>
+          </div>
+        </div>
+      </>
+    </BrowserRouter>
   );
 
   function toggleSidenav() {
     setSidenavOpen(!sidenavOpen);
   }
 }
+
+type NavProps = {
+  history?: any;
+};
 
 export default openmrsRootDecorator({ featureName: "primary navigation" })(
   Root
