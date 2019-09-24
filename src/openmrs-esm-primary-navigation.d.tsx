9457d4c46f780e34b2e8c518d8ed4205fa9d50b2
Joel Denning <joeldenning@gmail.com>
2019-09-23 21:20:56 -0600

    MF-66 and MF-67. (#7)

--- src/openmrs-esm-primary-navigation.d.tsx
+++
@@ -1,9 +0,0 @@
-// This entire file will be deleted once we have typescript definitions
-// for @openmrs/esm-api published to npm
-
-declare module "@openmrs/esm-api" {
-  export function openmrsFetch(url: string, fetchInit: any): any;
-  export function getCurrentUser(opts?: any): any;
-}
-
-declare module "*.css";
