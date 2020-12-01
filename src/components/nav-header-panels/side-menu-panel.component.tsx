ee4e600bcc35f2a140a9bbe692d5a5280857c70c
Brandon Istenes <bistenes@gmail.com>
2020-11-30 17:02:46 -0800

    Make left nav slot sensitive to activePage

--- src/components/nav-header-panels/side-menu-panel.component.tsx
+++ src/components/nav-header-panels/side-menu-panel.component.tsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SideNav, SideNavProps } from "carbon-components-react";
 import { ExtensionSlot } from "@openmrs/esm-react-utils";
+import { getAppState } from "@openmrs/esm-api";
 
 interface SideMenuPanelProps extends SideNavProps {}
 
 const SideMenuPanel: React.FC<SideMenuPanelProps> = ({ expanded }) => {
+  const [activePage, setActivePage] = useState();
+
+  useEffect(() => {
+    setActivePage(getAppState().getState().activePage);
+    return getAppState().subscribe(v => {
+      setActivePage(v.activePage);
+    });
+  }, []);
+
   return (
     expanded && (
       <SideNav expanded aria-label="Menu">
-        <ExtensionSlot extensionSlotName="nav-menu" />
+        {activePage && (
+          <ExtensionSlot extensionSlotName={`nav-menu-${activePage}`} />
+        )}
       </SideNav>
     )
   );
