fbe8ef412eeb7e25124d24b012c44cade46c643f
Donald Kibet <chelashawdonald@yahoo.com>
2021-02-12 12:34:02 +0300

    Add brand color on side-menu links on focus and hover

--- src/components/logo.component.tsx
+++ src/components/logo.component.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
 
-const Logo = ({ width = "110", height = "30" }) => {
+const Logo = ({ width = "110", height = "40" }) => {
   return (
     <svg role="img" width={width} height={height}>
-      <use xlinkHref="#omrs-logo-full-grey"></use>
+      <use xlinkHref="#omrs-logo-partial-grey"></use>
     </svg>
   );
 };
