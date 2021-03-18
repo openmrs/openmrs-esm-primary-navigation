32697383856ac553a6ff61835857b191570055b2
Joachim Junior <you@example.com>
2021-03-18 22:31:56 +0100

    Removed absolute URLs and Replaced with Dynamic

--- src/nav-options.tsx
+++ src/nav-options.tsx
@@ -1,43 +1,43 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import singleSpaReact from "single-spa-react";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import singleSpaReact from 'single-spa-react';
 import {
   HeaderPanel,
   Switcher,
   SwitcherItem,
-  SwitcherDivider
-} from "carbon-components-react/es/components/UIShell";
+  SwitcherDivider,
+} from 'carbon-components-react/es/components/UIShell';
 
 export default singleSpaReact({
   React,
   ReactDOM,
-  rootComponent: AppNavButton
+  rootComponent: AppNavButton,
 });
 
 export function AppNavButton() {
   const SourceFiles = {
-    clinical: "http://localhost:8080/openmrs/spa/home",
-    patients: "http://localhost:8080/openmrs/spa/patient-registration",
-    reports: "http://localhost:8080/openmrs/reportingui/reportsapp/home.page",
-    schedule: "http://localhost:8080/openmrs/appointmentschedulingui/home.page"
+    clinical: '${openmrsSpaBase}/home',
+    patients: '${openmrsSpaBase}/patient-registration',
+    reports: '${openmrsSpaBase}/reportingui/reportsapp/home.page',
+    schedule: '${openmrsSpaBase}/appointmentschedulingui/home.page',
   };
   const options = (
-    <HeaderPanel aria-label="Header Panel" expanded>
-      <Switcher aria-label="Switcher Container">
+    <HeaderPanel aria-label='Header Panel' expanded>
+      <Switcher aria-label='Switcher Container'>
         <SwitcherItem
-          aria-label="Clinical Dashboard"
+          aria-label='Clinical Dashboard'
           href={SourceFiles.clinical}
         >
           Clinical Dashboard
         </SwitcherItem>
         <SwitcherDivider />
-        <SwitcherItem href={SourceFiles.patients} aria-label="Patients">
+        <SwitcherItem href={SourceFiles.patients} aria-label='Patients'>
           Patients
         </SwitcherItem>
-        <SwitcherItem href={SourceFiles.schedule} aria-label="Schedule">
+        <SwitcherItem href={SourceFiles.schedule} aria-label='Schedule'>
           Schedule
         </SwitcherItem>
-        <SwitcherItem href={SourceFiles.reports} aria-label="Reports">
+        <SwitcherItem href={SourceFiles.reports} aria-label='Reports'>
           Reports
         </SwitcherItem>
       </Switcher>
